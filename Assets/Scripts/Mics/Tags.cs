8591b88a50c2eaa1a32e37f7bfb2f8ecbb752070
Soraku7 <2673140287@qq.com>
2026-05-18 01:43:31 +0800

    Add RoadChange

---
+++ Assets/Scripts/Mics/Tags.cs
@@ -0,0 +1,8 @@
+﻿namespace Mics
+{
+    public static class Tags
+    {
+        public const string Player = "Player";
+        public const string Road = "Road";
+    }
+}
---
+++ Assets/Scripts/RoadChange.cs
@@ -0,0 +1,29 @@
+﻿using Mics;
+using UnityEngine;
+
+public class RoadChange : MonoBehaviour
+{
+    public GameObject[] pattern;
+    public GameObject nowPattern;
+    public GameObject nextPattern;
+
+    private void Start()
+    {
+        nowPattern = Instantiate(pattern[0]);
+        nowPattern.transform.position = Vector3.zero;
+        nextPattern = Instantiate(pattern[1]);
+        nextPattern.transform.position = Vector3.zero + Vector3.forward * 160;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Tags.Road))
+        {
+            int index = Random.Range(0, pattern.Length);
+            GameObject patternInstance = GameObject.Instantiate(pattern[index]);
+            patternInstance.transform.position = nextPattern.transform.position + Vector3.forward * 160;
+            nowPattern = nextPattern;
+            nextPattern = patternInstance;
+        }
+    }
+}
