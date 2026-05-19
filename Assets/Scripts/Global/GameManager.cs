c8abffb50e23b3037a1444152d6fa0674ca99cd0
Soraku7 <2673140287@qq.com>
2026-05-20 01:50:50 +0800

    Add GameManager

---
+++ Assets/Scripts/Global/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Global
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static Sound sound;
+
+        private void Awake()
+        {
+            sound = gameObject.AddComponent<Sound>();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
