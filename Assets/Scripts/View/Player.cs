7f3a27e81496022d73c540ff67145ccc21acdf0e
Soraku7 <2673140287@qq.com>
2026-05-25 01:42:19 +0800

    Add touch-based movement control

--- Assets/Scripts/View/Player.cs
+++ Assets/Scripts/View/Player.cs
@@ -7,6 +7,8 @@ namespace Player
     {
         private CharacterController _characterController;
         private float _runSpeed = 10;
+        private Vector2 touchPos;
+        private float maxSqrDistance = 225;
 
         private void Awake()
         {
@@ -16,6 +18,44 @@ namespace Player
         public void FixedUpdate()
         {
             _characterController.Move(_runSpeed * Time.deltaTime * transform.forward);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                touchPos = Input.mousePosition;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                Vector2 finPos = Input.mousePosition;
+                Vector2 dir = finPos - touchPos;
+                if (dir.sqrMagnitude > maxSqrDistance)
+                {
+                    if (dir.x > dir.y)
+                    {
+                        if (dir.x > 0)
+                        {
+                            //右
+                        }
+                        else if (dir.x < 0)
+                        {
+                            //左
+                        }
+                    }
+                    else if (dir.x < dir.y)
+                    {
+                        if (dir.y > 0)
+                        {
+                            //上
+                        }
+                        else if (dir.y < 0)
+                        {
+                            //下
+                        }
+                    }
+                }
+            }
         }
 
         public override string Name => Consts.V_Player;
