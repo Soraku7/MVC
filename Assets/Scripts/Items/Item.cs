46d00602ea2dd518c48ae5c6c2d406a119fcd60d
Soraku7 <2673140287@qq.com>
2026-08-03 08:18:33 +0800

    fix bug

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -5,7 +5,6 @@ namespace Items
 {
     public abstract class Item : ReusableItem
     {
-        public int angular = 180;
         protected virtual void LateUpdate()
         {
             transform.Rotate(Vector3.up * (angular * Time.deltaTime));
